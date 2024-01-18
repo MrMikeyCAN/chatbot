c0a41b6e84c83f14bb01a512b29bf0b6979b21b7
mertcan <kadirmertcan2008@gmail.com>
2024-01-18 13:35:41 +0300

    versions 2.1 bugfixes/Users/mertcan/anaconda3/bin/python

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 from gtts import gTTS
 
 
-def text_to_spech(text: str, lang="en"):
+def text_to_spech(bot_name: str, text: str, lang="en"):
     tts = gTTS(text=text, lang=lang)
     filename = "sound.mp3"
     tts.save(filename)
+    print(f"{bot_name}: {text}")
