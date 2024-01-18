9df624d51710f9dcfc19ba091a22c52901396ab5
mertcan <kadirmertcan2008@gmail.com>
2024-01-18 16:20:19 +0300

    Error handling

--- chat.py
+++ chat.py
@@ -4,6 +4,7 @@ from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 from torch.nn.utils.rnn import pad_sequence
 from torch.nn.functional import softmax
+from utils import text_to_speech
 
 
 class YourTextGenerationModel(torch.nn.Module):
@@ -20,14 +21,14 @@ class YourTextGenerationModel(torch.nn.Module):
         return output
 
     def generate_text(self, start_sequence, max_length=50, all_words=None, device=None):
-        generated_sequence = start_sequence.copy_()
+        generated_sequence = start_sequence.clone()
 
         for _ in range(max_length):
             input_sequence = torch.LongTensor(
                 [
-                    start_sequence.index(word)
+                    start_sequence.tolist().index(word)
                     for word in generated_sequence
-                    if word in start_sequence
+                    if word in start_sequence.tolist()
                 ]
             )
 
@@ -46,14 +47,15 @@ class YourTextGenerationModel(torch.nn.Module):
             else:
                 break  # Break if the input sequence is empty
 
+        generated_sequence = [str(word) for word in generated_sequence if str(word) != '0.0']
+        
         return " ".join(generated_sequence)
 
 
 class Chatbot:
     def __init__(self, config):
-        self.device = torch.device(
-            config.get("device", "cuda" if torch.cuda.is_available() else "cpu")
-        )
+        cuda_available = torch.cuda.is_available() and config.get("device", "cuda")
+        self.device = torch.device("cuda" if cuda_available else "cpu")
         self.all_words = None
         self.tags = None
         self.intents = None  # Add this line
@@ -94,23 +96,32 @@ class Chatbot:
         tag = self.tags[predicted.item()]
 
         if tag == "goodbye":
-            print("I am waiting for your orders sir!")
+            text_to_speech(
+                bot_name=self.bot_name, text="I am waiting for your orders sir!"
+            )
             exit()
 
         probs = softmax(output, dim=1)
         prob = probs[0][predicted.item()]
 
+        print(f"Tag: {tag}, Probability: {prob.item()}")
+
         if prob.item() > 0.85:
             for intent in self.intents.get("intents", []):
                 if tag == intent.get("tag"):
                     generated_response = self.text_gen_model.generate_text(
-                        start_sequence
+                        start_sequence, all_words=self.all_words, device=self.device
                     )
-                    print(generated_response)
+                    print(f"Generated Response: {generated_response}")
+                    text_to_speech(bot_name=self.bot_name, text=generated_response)
         else:
             start_sequence = self.process_input(input_text)  # Use the processed input
-            generated_response = self.text_gen_model.generate_text(start_sequence)
-            print(generated_response)
+            generated_response = self.text_gen_model.generate_text(
+                start_sequence, all_words=self.all_words, device=self.device
+            )
+            print(f"Generated Response: {generated_response}")
+            text_to_speech(bot_name=self.bot_name, text=generated_response)
+
 
     def run(self):
         start_sequence = []
