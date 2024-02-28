ca1e67fec777a71dab41a528890ebfd9c1cde09e
mertcan <kadirmertcan2008@gmail.com>
2024-02-28 21:37:08 +0300

    training arguments classı bulundu

--- models/OurModel.py
+++ models/OurModel.py
@@ -2,20 +2,49 @@ import torch
 import torch.nn as nn
 
 
-### TODO Decoder ve Transformers classlarında kullanmak için AttentionBlock oluşturuyoruz (Mertin değimiyle en zor tek kısım)
+class TrainArgumentsForOurModel:
+    def __init__(
+        self,
+        embed_size=512,
+        heads=8,
+        dropout=0,
+        forward_expansion=4,
+        src_vocab_size=None,
+        trg_vocab_size=None,
+        src_pad_idx=0,
+        trg_pad_idx=0,
+        num_layers=6,
+        device="cpu",
+        max_length=100,
+        norm_activite_func="LayerNorm",
+    ):
+        self.embed_size = embed_size
+        self.heads = heads
+        self.dropout = dropout
+        self.forward_expansion = forward_expansion
+        self.norm_activite_func = norm_activite_func
+        self.src_vocab_size = src_vocab_size
+        self.trg_vocab_size = trg_vocab_size
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.num_layers = num_layers
+        self.device = device
+        self.max_length = max_length
+
+
 class AttentionBlock(nn.Module):
     def __init__(self, embed_size, heads):
-        super(AttentionBlock, self).__init__()  # Yapıcı yöntem çağrısı eklendi
+        super(AttentionBlock, self).__init__()
         self.embed_size = embed_size
         self.heads = heads
         self.head_dim = embed_size // heads
 
-        # * Gömülü boyutların heads sayısına bölünebilir olduğunu kontrol ediyoruz
+        # Başlıklara göre gömülü boyutlarının uygun olup olmadığını kontrol edin
         assert (
             self.head_dim * heads == embed_size
         ), "Gömülü değer belirtilen başlıklara bölünebilmeli"
 
-        # * v,q,k,o değerlerini oluşturuyoruz
+        # Anahtar, değer ve sorgu matrislerini oluşturun
         self.values = nn.Linear(embed_size, embed_size)
         self.keys = nn.Linear(embed_size, embed_size)
         self.queries = nn.Linear(embed_size, embed_size)
@@ -23,103 +52,64 @@ class AttentionBlock(nn.Module):
 
     def forward(self, values, keys, query, mask):
         N = query.shape[0]
-
-        # * Değerlerin boyutlarını alıyoruz
         value_len, key_len, query_len = (
             values.shape[1],
             keys.shape[1],
             query.shape[1],
         )
 
-        # * Değerleri ilgili katmanlardan geçiriyoruz
+        # Değerlerin boyutunu alın
         values = self.values(values)
         keys = self.keys(keys)
         queries = self.queries(query)
 
-        # * Başlıklara göre gömülü değerleri ayırıyoruz
+        # Boyutları yeniden şekillendirin
         values = values.reshape(N, value_len, self.heads, self.head_dim)
         keys = keys.reshape(N, key_len, self.heads, self.head_dim)
         queries = queries.reshape(N, query_len, self.heads, self.head_dim)
 
-        # * Attention hesabı için query*key işlemini yapıyoruz
+        # Sorgu ve anahtar arasındaki dikkat ağırlıklarını hesaplayın
         energy = torch.einsum("nqhd,nkhd->nhqk", [queries, keys])
 
-        # * Mask işlemini gerçekleştiriyoruz
+        # Maskeyi uygulayın (varsa)
         if mask is not None:
             energy = energy.masked_fill(mask == 0, float("-1e20"))
 
-        # * Energy değerlerini softmax ile normalize ediyoruz
+        # Dikkat ağırlıklarını softmax ile normalize edin
         attention = torch.softmax(energy / (self.embed_size ** (1 / 2)), dim=3)
 
-        # * Son çıktıyı hesaplıyoruz
+        # Ağırlıklı değerlerle birleştirin
         out = torch.einsum("nhql,nlhd->nqhd", [attention, values]).reshape(
             N, query_len, self.heads * self.head_dim
         )
 
+        # Çıktıyı tam bağlı katmana gönderin
         out = self.fc_out(out)
 
         return out
 
 
-### * Norm işlemleri için farklı iki yol
-
-
-# TODO GELU aktivasyon fonksiyonunu kullanalım
 class Tanh(nn.Module):
     def forward(self, x):
         return 0.5 * x * (1 + torch.tanh(0.79788456 * (x + 0.044715 * x**3)))
 
 
-# # TODO Group Normalizasyon katmanını kullanalım
-# class GroupNormBlock(nn.Module):
-#     def __init__(self, embed_size, num_groups=8):
-#         super(GroupNormBlock, self).__init__()
-#         if embed_size % num_groups != 0:
-#             raise ValueError("embed_size should be divisible by num_groups.")
-#         if embed_size < num_groups:
-#             raise ValueError("embed_size should be greater than or equal to num_groups.")
-#         self.norm = nn.GroupNorm(num_groups, embed_size)
-
-#     def forward(self, x):
-#         return self.norm(x)
-
-
-### TODO Transformer block sadece bağlantıyı es geçmek için ve normalize işlemlerini gerçekleştirmek için oluşturduğumuz blok parçası
 class TransformerBlock(nn.Module):
-    def __init__(
-        self,
-        embed_size,
-        heads,
-        dropout,
-        forward_expansion,
-        norm_activite_func="LayerNorm",
-    ):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
         super(TransformerBlock, self).__init__()
         self.attention = AttentionBlock(embed_size, heads)
-        if norm_activite_func.lower() == "layernorm":
-            self.norm1 = nn.LayerNorm(embed_size)
-            self.norm2 = nn.LayerNorm(embed_size)
-        elif norm_activite_func.lower() == "tanh":  # elif kullanılmalı
-            self.norm1 = Tanh()
-            self.norm2 = Tanh()
-        # elif norm_activite_func == "Group":  # elif kullanılmalı
-        #     self.norm1 = GroupNormBlock(embed_size)
-        #     self.norm2 = GroupNormBlock(embed_size)
-        else:
-            raise ValueError("Geçersiz norm_activite_func değeri")
-
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
         self.feed_forward = nn.Sequential(
             nn.Linear(embed_size, forward_expansion * embed_size),
             nn.ReLU(),
             nn.Linear(forward_expansion * embed_size, embed_size),
         )
-
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, value, key, query, mask):
+        # Dikkat mekanizmasını ve katman normalizasyonunu uygulayın
         attention = self.attention(value, key, query, mask)
-
-        # TODO Bağlantıyı es geçmek ve direkt norm işlemlerine uğramak için dropout ekliyoruz
         x = self.dropout(self.norm1(attention + query))
         forward = self.feed_forward(x)
         out = self.dropout(self.norm2(forward + x))
@@ -166,7 +156,7 @@ class Encoder(nn.Module):
             (self.word_embedding(x) + self.position_embedding(positions))
         )
 
-        # ! Encoder içerisinde Key Value ve Query AYNI olacaktır ,Decoder içerisinde değişikliğe uğrayacaktır
+        # Tüm katmanları geçişli olarak uygulayın
         for layer in self.layers:
             out = layer(out, out, out, mask)
 
@@ -191,7 +181,6 @@ class DecoderBlock(nn.Module):
             heads,
             dropout,
             forward_expansion,
-            norm_activite_func=norm_activite_func,
         )
         self.dropout = nn.Dropout(dropout)
 
@@ -200,9 +189,6 @@ class DecoderBlock(nn.Module):
         query = self.dropout(self.norm(attention + x))
         out = self.transformer_block(value, key, query, src_mask)
         return out
-
-
-### * Bu 3 block tamamlandıktan sonra geriye sadece onları decoder ve transformers class içerisinde birleştirmek kalıyor
 
 
 class Decoder(nn.Module):
@@ -231,7 +217,6 @@ class Decoder(nn.Module):
                     forward_expansion,
                     dropout,
                     device,
-                    norm_activite_func,
                 )
                 for _ in range(num_layers)
             ]
@@ -253,56 +238,39 @@ class Decoder(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(
-        self,
-        src_vocab_size,
-        trg_vocab_size,
-        src_pad_idx,
-        trg_pad_idx,
-        embed_size=512,
-        num_layers=6,
-        forward_expansion=4,
-        heads=8,
-        dropout=0,
-        # * Hata vermemesi için belirlenmese bile cpu'da çalışır halde düzenlendi
-        device="cpu",
-        max_length=100,
-        # * Varsayılan değer atıyoruz
-        norm_activite_func="LayerNorm",
-    ):
+    def __init__(self, arguments: TrainArgumentsForOurModel):
 
         super(Transformer, self).__init__()
 
         self.encoder = Encoder(
-            src_vocab_size,
-            embed_size,
-            num_layers,
-            heads,
-            device,
-            forward_expansion,
-            dropout,
-            max_length,
+            arguments.src_vocab_size,
+            arguments.embed_size,
+            arguments.num_layers,
+            arguments.heads,
+            arguments.device,
+            arguments.forward_expansion,
+            arguments.dropout,
+            arguments.max_length,
         )
 
         self.decoder = Decoder(
-            trg_vocab_size,
-            embed_size,
-            num_layers,
-            heads,
-            forward_expansion,
-            dropout,
-            device,
-            max_length,
-            norm_activite_func,
+            arguments.trg_vocab_size,
+            arguments.embed_size,
+            arguments.num_layers,
+            arguments.heads,
+            arguments.forward_expansion,
+            arguments.dropout,
+            arguments.device,
+            arguments.max_length,
+            arguments.norm_activite_func,
         )
 
-        self.src_pad_idx = src_pad_idx
-        self.trg_pad_idx = trg_pad_idx
-        self.device = device
+        self.src_pad_idx = arguments.src_pad_idx
+        self.trg_pad_idx = arguments.trg_pad_idx
+        self.device = arguments.device
 
     def make_src_mask(self, src):
         src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
-        # (N, 1, 1, src_len)
         return src_mask.to(self.device)
 
     def make_trg_mask(self, trg):
@@ -334,13 +302,20 @@ if __name__ == "__main__":
     trg_pad_idx = 0
     src_vocab_size = 10
     trg_vocab_size = 10
-    model = Transformer(
-        src_vocab_size,
-        trg_vocab_size,
-        src_pad_idx,
-        trg_pad_idx,
+    trainArguments = TrainArgumentsForOurModel(
+        embed_size=512,
+        heads=8,
+        dropout=0,
+        forward_expansion=4,
+        src_vocab_size=src_vocab_size,
+        trg_vocab_size=trg_vocab_size,
+        src_pad_idx=src_pad_idx,
+        trg_pad_idx=trg_pad_idx,
+        num_layers=6,
         device=device,
-        norm_activite_func="LayerNorm",
-    ).to(device)
+        max_length=100,
+        norm_activite_func="tanh",
+    )
+    model = Transformer(trainArguments).to(device)
     out = model(x, trg[:, :-1])
     print(out.shape)
