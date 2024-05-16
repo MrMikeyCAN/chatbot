4120a4cb8aeedabd2f3a77f40ba36f095a4269e5
mertcan <kadirmertcan2008@gmail.com>
2024-05-16 18:16:37 +0300

    GPT model kodları temizlendi ve düzenlendi

--- Test.py
+++ Test.py
@@ -1,11 +1,8 @@
-from models.GPTModel import (
-    Hyperparameters,
-    TrainParameters,
-    GPTLanguageModel,
-    ModelFuncs,
-)
-
+from models.GPTModel import GPTLanguageModel
 import torch
+import time
+import matplotlib.pyplot as plt
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -18,49 +15,154 @@ with open("input2.txt", "r", encoding="utf-8") as f:
 words = text.split()
 vocab_size = len(set(words))
 # Create a mapping from words to integers
-stoi = {w: i for i, w in enumerate(set(words))}
-itos = {i: w for i, w in enumerate(set(words))}
+sentences_to_indices = {w: i for i, w in enumerate(set(words))}
+indices_to_sentences = {i: w for i, w in enumerate(set(words))}
 
 
 def encode(sentence):
-    return [stoi[word] for word in sentence.split()]
+    return [sentences_to_indices[word] for word in sentence.split()]
 
 
 def decode(indices):
-    return " ".join(itos[i] for i in indices)
+    return " ".join(indices_to_sentences[i] for i in indices)
 
 
-# Define the hyperparameters
-hyperParams = Hyperparameters(
-    vocab_size=vocab_size,
-    n_embd=256,
-    n_head=6,
-    n_layer=20,
-    dropout=0.3,
-    batch_size=128,
-    block_size=128,
-    decoder=decode,
-    encoder=encode,
-    device=device,
-)
+# Hyper params
+vocab_size = vocab_size
+n_embd = 256
+n_head = 6
+n_layer = 20
+dropout = 0.3
+batch_size = 128
+block_size = 128
+decoder = decode
+encoder = encode
+device = device
 
-trainParams = TrainParameters(
-    learning_rate=2e-4,
-    device=device,
-    max_iters=5000,
-    checkpoint=10,
-    decoder=decode,
-    encoder=encode,
-    eval_interval=1,
-    eval_iters=100,
-    visualate=False,
-    text=text,
-)
+# Training params
+learning_rate = 3e-4
+device = device
+max_iters = 5000
+data = torch.tensor(encoder(text), dtype=torch.long)
+n = int(0.9 * len(data))
+train_data = data[: n]
+val_data = data[n:]
+checkpoint = 10
+eval_interval = 1
+eval_iters = 100
+graficate = False
+text = text
 
-model = GPTLanguageModel(hyperParams).to(device)
-modelFuncs = ModelFuncs(hyperparams=hyperParams, train_params=trainParams, model=model)
-modelFuncs.m = modelFuncs.m.to(device)
+model = GPTLanguageModel(block_size, n_embd, decoder, n_head, n_layer, vocab_size, dropout).to(device)
+m = model.to(device)
 print("Device of model:", next(model.parameters()).device)
-print("Train parameters device:", trainParams.device)
-print("Hyper parameters device:", hyperParams.device)
-modelFuncs.train()
+print("Train parameters device:", device)
+print("Hyper parameters device:", device)
+
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    _data = train_data if split == "train" else val_data
+    ix = torch.randint(len(_data) - block_size, (batch_size,))
+    x = torch.stack([data[i: i + block_size] for i in ix]).to(device)
+    y = torch.stack([data[i + 1: i + block_size + 1] for i in ix]).to(device)
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+
+def generate_text(
+        context: str,
+        max_new_tokens: int = 500,
+):
+    generated_text = ""
+    context = torch.tensor(encoder(context), device=device)[
+              None, :
+              ]
+    generated_text += decoder(
+        m.generate(context, max_new_tokens)[0].tolist()
+    )
+    return generated_text
+
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ["train", "val"]:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            x, y = get_batch(split)
+            logits, loss = model(x, y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+def train():
+    dir_name = f"checkpoints/{time.time()}"
+    train_losses = []
+    val_losses = []
+    iter_values = []
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=learning_rate
+    )
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, max_iters
+    )
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    print(sum(p.numel() for p in m.parameters()) / 1e6, "M parameters")
+    for iters in range(max_iters):
+        start_time = time.time()
+
+        # every once in a while evaluate the loss on train and val sets
+        if iters % eval_interval == 0 or iters == max_iters - 1:
+            losses = estimate_loss()
+            train_loss = losses["train"]
+            val_loss = losses["val"]
+            print(
+                f"step {iters}: train loss {train_loss:.4f}, val loss {val_loss:.4f}"
+            )
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            iter_values.append(iter)
+            print(generate_text("my name is", 5))
+
+            if iter != 0 and checkpoint != 0 and iters % checkpoint == 0:
+                path_name = dir_name + f"/checkpoint:{iters}.h5"
+                torch.save(
+                    model.state_dict(),
+                    path_name,
+                )
+                print("checkpoint successfully saved")
+
+            print("------------------------------------")
+
+        # sample a batch of data
+        xb, yb = get_batch("train")
+
+        # evaluate the loss
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        end_time = time.time()
+        print(f"Time elapsed: {end_time - start_time}")
+
+    # Plot losses
+    if graficate:
+        plt.plot(iter_values, train_losses, label="Train Loss")
+        plt.plot(iter_values, val_losses, label="Val Loss")
+        plt.xlabel("Iterations")
+        plt.ylabel("Loss")
+        plt.title("Train and Validation Loss")
+        plt.legend()
+        plt.show()
+    torch.save(model.state_dict(), "model_weights.pth")
+    print("Model weights saved successfully")
+
+
+train()
--- models/GPTModel.py
+++ models/GPTModel.py
@@ -1,48 +1,11 @@
 import math
-import time
-
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
-
-import os
+from torch.nn import functional as f
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-
 # ! Hyper parametreler model için gerekli tüm parametreleri içerir ve de bir çok gerekli kodu
-class Hyperparameters:
-    def __init__(
-            self,
-            batch_size,
-            block_size,
-            n_embd,
-            n_head,
-            n_layer,
-            dropout,
-            vocab_size,
-            encoder,
-            decoder,
-            device,
-    ):
-        # * Batch size
-        self.batch_size = batch_size
-        # * Block size
-        self.block_size = block_size
-        # * Gömülü katman sayısı
-        self.n_embd = n_embd
-        self.n_head = n_head
-        # * Katman sayısı (Kaç farklı sonuç sallayacağını seçer)
-        self.n_layer = n_layer
-        self.dropout = dropout
-        self.vocab_size = vocab_size
-        self.encoder = encoder
-        self.decoder = decoder
-        self.device = device
-
-    # hyperparameters
-
 
 # ------------
 
@@ -50,44 +13,7 @@ torch.manual_seed(42)
 
 
 # ! Eğitim için gerekli tüm kodları içerir
-class TrainParameters:
-    def __init__(
-            self,
-            text,
-            max_iters,
-            eval_interval,
-            learning_rate,
-            device,
-            eval_iters,
-            checkpoint: int,
-            visualate: bool,
-            encoder,
-            decoder,
-    ):
-        self.encoder = encoder
-        self.decoder = decoder
-        self.data = torch.tensor(self.encoder(text), dtype=torch.long)
-        self.n = int(0.9 * len(self.data))
-        self.train_data = self.data[: self.n]
-        self.val_data = self.data[self.n:]
-        self.max_iters = max_iters
-        self.eval_interval = eval_interval
-        self.learning_rate = learning_rate
-        self.device = device
-        self.eval_iters = eval_iters
-        self.checkpoint = checkpoint
-        self.visualate = visualate
 
-
-# data loading
-def get_batch(split, hyperparams: Hyperparameters, trainParameters: TrainParameters):
-    # generate a small batch of data of inputs x and targets y
-    data = trainParameters.train_data if split == "train" else trainParameters.val_data
-    ix = torch.randint(len(data) - hyperparams.block_size, (hyperparams.batch_size,))
-    x = torch.stack([data[i: i + hyperparams.block_size] for i in ix]).to(device)
-    y = torch.stack([data[i + 1: i + hyperparams.block_size + 1] for i in ix]).to(device)
-    x, y = x.to(trainParameters.device), y.to(trainParameters.device)
-    return x, y
 
 
 class Head(nn.Module):
@@ -106,15 +32,15 @@ class Head(nn.Module):
     def forward(self, x):
         # input of size (batch, time-step, channels)
         # output of size (batch, time-step, head size)
-        B, T, C = x.shape
+        b, t, c = x.shape
         k = self.key(x)  # (B,T,hs)
         q = self.query(x)  # (B,T,hs)
         # compute attention scores ("affinities")
         wei = (
                 q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5
         )  # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # (B, T, T)
-        wei = F.softmax(wei, dim=-1)  # (B, T, T)
+        wei = wei.masked_fill(self.tril[:t, :t] == 0, float("-inf"))  # (B, T, T)
+        wei = f.softmax(wei, dim=-1)  # (B, T, T)
         wei = self.dropout(wei)
         # perform the weighted aggregation of the values
         v = self.value(x)  # (B,T,hs)
@@ -187,12 +113,37 @@ class Block(nn.Module):
         return x
 
 
-class PositionalEncoding:
-    def __init__(self, block_size, n_embd):
-        self.block_size = block_size
-        self.n_embd = n_embd
+class GPTLanguageModel(nn.Module):
 
-    def _generate_positional_encoding(block_size, n_embd):
+    def __init__(self, block_size, n_embd, decoder, n_head, n_layer, vocab_size, dropout):
+        super().__init__()
+        self.block_size = block_size
+        self.positional_encoding = self._generate_positional_encoding(block_size=block_size, n_embd=n_embd)
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(
+            self.block_size, n_embd
+        )
+        self.decoder = decoder
+        self.blocks = nn.Sequential(
+            *[
+                Block(
+                    n_embd=n_embd,
+                    n_head=n_head,
+                    block_size=block_size,
+                    dropout=dropout,
+                )
+                for _ in range(n_layer)
+            ]
+        )
+        self.ln_f = nn.LayerNorm(n_embd)  # final layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self.__init__weights)
+
+    @staticmethod
+    def _generate_positional_encoding(block_size: int, n_embd: int) -> torch.Tensor:
         position = torch.arange(block_size).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, n_embd, 2) * -(math.log(10000.0) / n_embd))
         positional_encoding = torch.zeros(block_size, n_embd)
@@ -200,41 +151,8 @@ class PositionalEncoding:
         positional_encoding[:, 1::2] = torch.cos(position * div_term)
         return nn.Parameter(positional_encoding, requires_grad=True)
 
-
-class GPTLanguageModel(nn.Module):
-
-    def __init__(self, hyperparams: Hyperparameters):
-        super().__init__()
-        self.block_size = hyperparams.block_size
-        self.positional_encoding = PositionalEncoding._generate_positional_encoding(
-            block_size=hyperparams.block_size, n_embd=hyperparams.n_embd
-        )
-        # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(
-            hyperparams.vocab_size, hyperparams.n_embd
-        )
-        self.position_embedding_table = nn.Embedding(
-            self.block_size, hyperparams.n_embd
-        )
-        self.decoder = hyperparams.decoder
-        self.blocks = nn.Sequential(
-            *[
-                Block(
-                    n_embd=hyperparams.n_embd,
-                    n_head=hyperparams.n_head,
-                    block_size=hyperparams.block_size,
-                    dropout=hyperparams.dropout,
-                )
-                for _ in range(hyperparams.n_layer)
-            ]
-        )
-        self.ln_f = nn.LayerNorm(hyperparams.n_embd)  # final layer norm
-        self.lm_head = nn.Linear(hyperparams.n_embd, hyperparams.vocab_size)
-
-        # better init, not covered in the original GPT video, but important, will cover in followup video
-        self.apply(self.__init__weights)
-
-    def __init__weights(self, module):
+    @staticmethod
+    def __init__weights(module):
         if isinstance(module, nn.Linear):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
             if module.bias is not None:
@@ -243,12 +161,12 @@ class GPTLanguageModel(nn.Module):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets=None):
-        B, T = idx.shape
+        b, t = idx.shape
 
         # idx and targets are both (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx)  # (B,T,C)
 
-        pos_emb = self.positional_encoding[:T, :].unsqueeze(0)  # (T,C)
+        pos_emb = self.positional_encoding[:t, :].unsqueeze(0)  # (T,C)
 
         x = tok_emb + pos_emb  # (B,T,C)
 
@@ -260,10 +178,10 @@ class GPTLanguageModel(nn.Module):
         if targets is None:
             loss = None
         else:
-            B, T, C = logits.shape
-            logits = logits.view(B * T, C)
-            targets = targets.view(B * T)
-            loss = F.cross_entropy(logits, targets)
+            b, t, c = logits.shape
+            logits = logits.view(b * t, c)
+            targets = targets.view(b * t)
+            loss = f.cross_entropy(logits, targets)
 
         return logits, loss
 
@@ -277,7 +195,7 @@ class GPTLanguageModel(nn.Module):
             # focus only on the last time step
             logits = logits[:, -1, :]  # becomes (B, C)
             # apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1)  # (B, C)
+            probs = f.softmax(logits, dim=-1)  # (B, C)
             # sample from the distribution
             idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
             # append sampled index to the running sequence
@@ -286,119 +204,3 @@ class GPTLanguageModel(nn.Module):
                 print("-------------FINISH-------------")
                 break
         return idx
-
-
-# ! Model ile ilgili tüm parametreler
-class ModelFuncs:
-    def __init__(
-            self,
-            model: GPTLanguageModel,
-            hyperparams: Hyperparameters,
-            train_params: TrainParameters,
-    ):
-        self.hyperparams = hyperparams
-        self.train_param = train_params
-        self.model = GPTLanguageModel(self.hyperparams).to(device)
-        self.m = model.to(device)
-
-    def Generate_Text(
-            self,
-            context: str,
-            max_new_tokens: int = 500,
-    ):
-        generated_text = ""
-        context = torch.tensor(self.hyperparams.encoder(context), device=device)[
-                  None, :
-                  ]
-        generated_text += self.hyperparams.decoder(
-            self.m.generate(context, max_new_tokens)[0].tolist()
-        )
-        return generated_text
-
-    @torch.no_grad()
-    def estimate_loss(self):
-        out = {}
-        self.model.eval()
-        for split in ["train", "val"]:
-            losses = torch.zeros(self.train_param.eval_iters)
-            for k in range(self.train_param.eval_iters):
-                X, Y = get_batch(
-                    split,
-                    trainParameters=self.train_param,
-                    hyperparams=self.hyperparams,
-                )
-                logits, loss = self.model(X, Y)
-                losses[k] = loss.item()
-            out[split] = losses.mean()
-        self.model.train()
-        return out
-
-    def train(self):
-        model = self.model
-        checkpoints = self.train_param.checkpoint
-        visualate = self.train_param.visualate
-        dirName = f"checkpoints/{time.time()}"
-        train_losses = []
-        val_losses = []
-        iter_values = []
-        optimizer = torch.optim.AdamW(
-            model.parameters(), lr=self.train_param.learning_rate
-        )
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            optimizer, T_max=self.train_param.max_iters
-        )
-        eval_interval = self.train_param.eval_interval
-        max_iters = self.train_param.max_iters
-        if not os.path.exists(dirName):
-            os.makedirs(dirName)
-
-        print(sum(p.numel() for p in self.m.parameters()) / 1e6, "M parameters")
-        for iter in range(self.train_param.max_iters):
-            start_time = time.time()
-
-            # every once in a while evaluate the loss on train and val sets
-            if iter % eval_interval == 0 or iter == max_iters - 1:
-                losses = self.estimate_loss()
-                train_loss = losses["train"]
-                val_loss = losses["val"]
-                print(
-                    f"step {iter}: train loss {train_loss:.4f}, val loss {val_loss:.4f}"
-                )
-                train_losses.append(train_loss)
-                val_losses.append(val_loss)
-                iter_values.append(iter)
-                print(self.Generate_Text("my name is", 5))
-
-                if iter != 0 and checkpoints != 0 and iter % checkpoints == 0:
-                    path_name = dirName + f"/checkpoint:{iter}.h5"
-                    torch.save(
-                        model.state_dict(),
-                        path_name,
-                    )
-                    print("checkpoint successfuly saved")
-
-                print("------------------------------------")
-
-            # sample a batch of data
-            xb, yb = get_batch("train", self.hyperparams, self.train_param)
-
-            # evaluate the loss
-            logits, loss = model(xb, yb)
-            optimizer.zero_grad(set_to_none=True)
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-            end_time = time.time()
-            print(f"Geçirilen zaman: {end_time - start_time}")
-
-        # Plot losses
-        if visualate:
-            plt.plot(iter_values, train_losses, label="Train Loss")
-            plt.plot(iter_values, val_losses, label="Val Loss")
-            plt.xlabel("Iterations")
-            plt.ylabel("Loss")
-            plt.title("Train and Validation Loss")
-            plt.legend()
-            plt.show()
-        torch.save(self.model.state_dict(), "model_weights.pth")
-        print("Model weights saved successfully")
